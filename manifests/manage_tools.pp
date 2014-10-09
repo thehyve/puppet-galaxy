56654e05270f5c648e54fd6b023d6fc0fc4db7ec
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-10-09 13:00:44 +0200

    Add unless to manage_tools run
    To avoid having a resource "change" unnecessarily.

--- manifests/manage_tools.pp
+++ manifests/manage_tools.pp
@@ -5,12 +5,15 @@
 #
 class galaxy::manage_tools {
   $directory = $galaxy::params::app_directory
+  $cmd = "python $directory/scripts/manage_tools.py"
   exec { 'Manage Tools Upgrade':
-  path    =>'/usr/bin:/usr/sbin:/bin:/sbin',
-  cwd     => $directory,
-  command => "python $directory/scripts/manage_tools.py upgrade",
-  user    => 'galaxy',
-  group   => 'galaxy',
-  require  => Class['galaxy::create_db'],
+    path     => '/usr/bin:/usr/sbin:/bin:/sbin',
+    cwd      => $directory,
+    command  => "$cmd upgrade",
+    user     => 'galaxy',
+    group    => 'galaxy',
+    unless   => "test \"\$($cmd db_version)\" -eq \"\$($cmd version)\"",
+    provider => shell,
+    require  => Class['galaxy::create_db'],
   }
 }
