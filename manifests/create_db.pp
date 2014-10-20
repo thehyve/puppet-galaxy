de80b2c9f5624c80a26a7a2e7bd64831a1801c8f
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-10-20 17:03:42 +0200

    Only run create_db.sh when data source changes
    Avoids being notified of an updated resource constantly; helps
    performance.

--- manifests/create_db.pp
+++ manifests/create_db.pp
@@ -5,14 +5,25 @@
 #
 #
 class galaxy::create_db {
+  include ::galaxy::universe
+
   $directory = $galaxy::params::app_directory
-  exec { 'create_db.sh':
-    require =>  Class ['galaxy::common_startup'],
-    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-    cwd     => $directory,
-    user    => 'galaxy',
+
+  # run create_db.sh only if the conection string changes
+  file { "$directory/.db_connection":
+    owner   => 'galaxy',
     group   => 'galaxy',
-    command => "$directory/create_db.sh",
-    timeout => 500,
+    mode    => '0600',
+    content => "$::galaxy::universe::db_connection\n",
+  } ~>
+  exec { 'create_db.sh':
+    require     => Class['galaxy::universe'],
+    path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+    cwd         => $directory,
+    user        => 'galaxy',
+    group       => 'galaxy',
+    command     => "$directory/create_db.sh",
+    timeout     => 500,
+    refreshonly => true,
   }
 }
