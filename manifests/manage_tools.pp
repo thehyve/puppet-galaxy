e05fcf8d9de171dedb9a8e64a5e3f63130a49387
Sietse Snel <sietse@thehyve.nl>
2018-02-14 11:29:50 +0100

    use venv for manage tools upgrade

--- manifests/manage_tools.pp
+++ manifests/manage_tools.pp
@@ -5,11 +5,11 @@
 #
 class galaxy::manage_tools {
   $directory = $galaxy::params::app_directory
-  $cmd = "python $directory/scripts/manage_tools.py"
+  $cmd = "python '$directory'/scripts/manage_tools.py"
   exec { 'Manage Tools Upgrade':
     path     => '/usr/bin:/usr/sbin:/bin:/sbin',
     cwd      => $directory,
-    command  => "$cmd upgrade",
+    command  => ". '$directory'/.venv/bin/activate && $cmd upgrade",
     user     => 'galaxy',
     group    => 'galaxy',
     unless   => "test \"\$($cmd db_version)\" -eq \"\$($cmd version)\"",
